53e53f72bef0a02c011264a3b963eed72fcdd185
jotals3 <jotal@napptilus.com>
2025-05-23 12:17:15 +0200

    added enabled by default fields

--- update.py
+++ update.py
@@ -17,6 +17,7 @@ file_template = {
       "desc": "Primary NetSamurai list that indicates the specific fragments of code to be removed in real time to avoid tracking and fingerprinting.",
       "path": "offsets/offsets.json",
       "format": "custom",
+      "enabled_default": True,
       "hash": ""  # SHA256
     },
     {
@@ -24,6 +25,7 @@ file_template = {
       "desc": "Supplementary NetSamurai list that removes paywalls for some of the most widely used domains in the region.",
       "path": "rules/paywalls.json",
       "format": "custom+regex",
+      "enabled_default": True,
       "hash": ""  # SHA256
     },
     {
@@ -31,6 +33,7 @@ file_template = {
       "desc": "Supplementary NetSamurai list that indicates cases in which the resources must not be modified, to avoid breaking functionality. To be removed in newer versions.",
       "path": "rules/unbreak.json",
       "format": "custom+regex",
+      "enabled_default": True,
       "hash": ""  # SHA256
     },
     {
@@ -38,6 +41,7 @@ file_template = {
       "desc": "Primary filter list that removes most adverts from international webpages, including unwanted frames, images and objects. The EasyList authors (https://easylist.to/).",
       "path": "adblock/easylist.txt",
       "format": "adblockplus",
+      "enabled_default": True,
       "hash": ""  # SHA256
     },
     {
@@ -45,6 +49,7 @@ file_template = {
       "desc": "Optional supplementary filter list that completely removes all forms of tracking from the internet, including web bugs, tracking scripts and information collectors. The EasyList authors (https://easylist.to/).",
       "path": "adblock/easyprivacy.txt",
       "format": "adblockplus",
+      "enabled_default": False,
       "hash": ""  # SHA256
     }
   ]
