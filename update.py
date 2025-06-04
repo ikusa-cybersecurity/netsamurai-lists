59d79c48e3ee0903a9f3cb05e5c641164031fe3a
jotals3 <jotal@napptilus.com>
2025-06-04 10:11:24 +0200

    added new fields title and mode to the lists

--- update.py
+++ update.py
@@ -14,6 +14,11 @@ file_template = {
     "lists": [
         {
             "name": "netsamurai-offsets",
+            "title": {
+                "en": "NetSamurai Offsets",
+                "es": "NetSamurai Offsets",
+                "ca": "NetSamurai Offsets"
+            },
             "desc": {
                 "en": "Primary NetSamurai list that indicates the specific fragments of code to be removed in real time to avoid tracking and fingerprinting.",
                 "es": "Lista principal de NetSamurai que indica los fragmentos de código específicos que deben eliminarse en tiempo real para evitar el seguimiento y la toma de huellas digitales.",
@@ -21,11 +26,17 @@ file_template = {
             },
             "path": "offsets/offsets.json",
             "format": "custom",
+            "mode": "cleanlist",
             "enabled_default": True,
             "hash": ""  # SHA256
         },
         {
             "name": "netsamurai-paywalls",
+            "title": {
+                "en": "NetSamurai Paywalls",
+                "es": "NetSamurai Paywalls",
+                "ca": "NetSamurai Paywalls"
+            },
             "desc": {
                 "en": "Supplementary NetSamurai list that removes paywalls for some of the most widely used domains in the region.",
                 "es": "Lista suplementaria de NetSamurai que elimina los muros de pago para algunos de los dominios más utilizados en la región.",
@@ -33,11 +44,17 @@ file_template = {
             },
             "path": "rules/paywalls.json",
             "format": "custom+regex",
+            "mode": "blocklist",
             "enabled_default": True,
             "hash": ""  # SHA256
         },
         {
             "name": "netsamurai-unbreak",
+            "title": {
+                "en": "NetSamurai Unbreak",
+                "es": "NetSamurai Unbreak",
+                "ca": "NetSamurai Unbreak"
+            },
             "desc": {
                 "en": "Supplementary NetSamurai list that indicates cases in which the resources must not be modified, to avoid breaking functionality. To be removed in newer versions.",
                 "es": "Lista suplementaria de NetSamurai que indica los casos en los que los recursos no deben modificarse para evitar romper la funcionalidad. Se eliminará en versiones posteriores.",
@@ -45,11 +62,17 @@ file_template = {
             },
             "path": "rules/unbreak.json",
             "format": "custom+regex",
+            "mode": "allowlist",
             "enabled_default": True,
             "hash": ""  # SHA256
         },
         {
             "name": "easylist",
+            "title": {
+                "en": "EasyList",
+                "es": "EasyList",
+                "ca": "EasyList"
+            },
             "desc": {
                 "en": "Primary filter list that removes most adverts from international webpages, including unwanted frames, images and objects. The EasyList authors: https://easylist.to/.",
                 "es": "Lista de filtros principal que elimina la mayoría de los anuncios de las páginas web internacionales, incluyendo marcos, imágenes y objetos no deseados. Los autores de EasyList: https://easylist.to/.",
@@ -57,11 +80,17 @@ file_template = {
             },
             "path": "adblock/easylist.txt",
             "format": "adblockplus",
+            "mode": "blocklist",
             "enabled_default": True,
             "hash": ""  # SHA256
         },
         {
             "name": "easyprivacy",
+            "title": {
+                "en": "EasyPrivacy",
+                "es": "EasyPrivacy",
+                "ca": "EasyPrivacy"
+            },
             "desc": {
                 "en": "Optional supplementary filter list that completely removes all forms of tracking from the internet, including web bugs, tracking scripts and information collectors. The EasyList authors: https://easylist.to/.",
                 "es": "Lista de filtros suplementaria opcional que elimina por completo todas las formas de seguimiento de internet, incluyendo web bugs, scripts de seguimiento y recolectores de información. Los autores de EasyList: https://easylist.to/.",
@@ -69,6 +98,7 @@ file_template = {
             }, 
             "path": "adblock/easyprivacy.txt",
             "format": "adblockplus",
+            "mode": "blocklist",
             "enabled_default": False,
             "hash": ""  # SHA256
         }
